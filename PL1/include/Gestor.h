f39d7c1d771c5db13adcb39eb7d8f3b89ba364c2
JgBGDelta <jorge.barroso@edu.uah.es>
2023-10-22 00:24:27 +0200

    Created Gestor class interface and created Lista Class

--- PL1/include/Gestor.h
+++ PL1/include/Gestor.h
@@ -7,10 +7,24 @@ class Gestor
     public:
         Gestor();
         virtual ~Gestor();
+        Cola generarColaReservas(); //Opción 1
+        void mostrarColaReservas(Cola colaReservas); //Opción 2
+        Cola vaciarColaReservas(); //Opción 3
+
+        Pila generarPisaMesas(); //Opción 4
+        void mostrarPilaMesas(); //Opción 5
+        Pila vaciarPilaReservas(); //Opción 6
+
+        void simularGestionPrimeraReserva(Cola colaReservas, Cola colaReservasPdtes, Lista listaPedidos); //Opción 7
+        void simularGestionReservasProximaHora(Cola colaReservas, Cola colaReservasPdtes, Lista listaPedidos); //Opción 8
+        void simularGestionReservasTotal(Cola colaReservas, Cola colaReservasPdtes, Lista listaPedidos); //Opción 9
+
+        void Salir(); //Opción 0
 
     protected:
 
     private:
+
 };
 
 #endif // GESTOR_H
