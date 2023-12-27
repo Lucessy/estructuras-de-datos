8fda199d5a298325f08f4cdd0ba3a2f3995317ab
JgBGDelta <jorge.barroso@edu.uah.es>
2023-12-27 02:05:23 +0100

    Commit extra sin nada

--- PL1/src/Gestor.cpp
+++ PL1/src/Gestor.cpp
@@ -483,7 +483,7 @@ void Gestor::Salir()
 
 
 /**
-* Inserta el pedido dado en la lista de pedidos
+* Inserta el pedido dado en la lista de pedidos.
 */
 void Gestor::insertarPedidoEnLista(Pedido* pedido)
 {
